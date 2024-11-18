abaab0508824951c069987e17162f2c90274a2ed
TOMOYA <jingcaoq@gmail.com>
2024-11-18 18:40:52 +0900

    Q1の問題を解きました。

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,1 +1,9 @@
-db_lesson.sql
+Q1.
+
+CREATE TABLE departments (
+    -> department_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    -> name VARCHAR(20) NOT NULL,
+    -> created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -> updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    -> );
+  
